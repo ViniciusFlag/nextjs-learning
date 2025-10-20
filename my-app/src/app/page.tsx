9258d2fc7bd17970a07b94a78fdc5a3efb133bfb
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2025-10-20 17:26:35 -0300

    add

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Acesso from "@/components/acesso";
+import ClientFetch from "@/components/client-fetch";
+import ServerFetch from "@/components/server-fetch";
 
 export default async function HomePage() {
   return (
     <main>
       <h1>Home</h1>
-      <Acesso />
+      {/* <Acesso /> */}
+
+      {/* <ServerFetch /> */}
+      <ClientFetch />
     </main>
   );
 }
