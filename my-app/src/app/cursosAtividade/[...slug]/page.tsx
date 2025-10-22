13243622ab59eb07c5563aa543bee6acc6343df8
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2025-10-22 16:44:36 -0300

    add

--- my-app/src/app/cursosAtividade/[...slug]/page.tsx
+++ my-app/src/app/cursosAtividade/[...slug]/page.tsx
@@ -1,7 +1,45 @@
-export default async function SlugCursoPage() {
+import Link from "next/link";
+
+type PageParams = {
+    params: {
+        slug: string[];
+    }
+}
+
+type Aula = {
+    id: number;
+    slug: string;
+    nome: string;
+    descricao: string;
+    curso_id: number;
+    tempo: number;
+    ordem: number;
+};
+
+type Curso = {
+    id: number;
+    slug: string;
+    nome: string;
+    descricao: string;
+    total_aulas: string;
+    total_horas: string;
+    aulas: Aula[];
+};
+
+export default async function SlugCursoPage({params}: PageParams) {
+    const response = await fetch(`https://api.origamid.online/cursos/${params.slug.join('/')}`);
+    const data = await response.json() as Curso;
+  
   return (
     <main>
-      <h1>Cursos</h1>
+      <h1>{data.nome}</h1>
+      <h3>{data.descricao}</h3>
+      <p>Aulas: {data.total_aulas} | Horas: {data.total_horas}</p>
+      <ul>
+        {data.aulas.map((aula) => (
+          <li key={aula.id}><Link href={`/cursosAtividade/${params.slug.join('/')}/${aula.slug}`}>{aula.nome}</Link></li>
+        ))}
+      </ul>
     </main>
   );
 }
--- my-app/src/app/cursosAtividade/page.tsx
+++ my-app/src/app/cursosAtividade/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type ParamsPage = {
     params: {
         id: string;
@@ -6,16 +8,25 @@ type ParamsPage = {
 
 type Curso = {
     id: number;
+    slug: string;
     nome: string;
     descricao: string;
+    total_aulas: string;
+    total_horas: string;
 };
 
 export default async function CursoAtividadePage({params}: ParamsPage) {
     const response = await fetch(`https://api.origamid.online/cursos`);
-    const data = await response.json() as Curso;
+    const data = await response.json() as Curso[];
+
   return (
     <main>
       <h1>Cursos</h1>
+      <ul>
+        {data.map((curso) => (
+          <li key={curso.id}> <Link href={`/cursosAtividade/${curso.slug}`}>{curso.nome}</Link></li>
+        ))}
+      </ul>
     </main>
   );
 }
